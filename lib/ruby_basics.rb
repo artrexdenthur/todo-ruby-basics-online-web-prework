6ac3a1bd158211f24201f07fad369eef2051b3ff
Paul Ashour <paul.ashour@gmail.com>
2018-09-30 23:30:10 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -13,13 +13,13 @@ end
 def greeting(arg1, arg2); end
 
 def return_a_value
-
+  'Nice'
 end
 
 def last_evaluated_value
-
+  'expert'
 end
 
-def pizza_party
-
+def pizza_party(val = 'cheese')
+  val
 end
