89dde49e4b7911f421e1af6617335f3bd231b559
Paul Ashour <paul.ashour@gmail.com>
2018-09-30 23:29:21 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -3,16 +3,14 @@ def division(num1, num2)
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
 def argue
-  
+  argue
 end
 
-def greeting
-
-end
+def greeting(arg1, arg2); end
 
 def return_a_value
 
